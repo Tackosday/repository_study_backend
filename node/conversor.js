5fe01ed226141ea97b4018ff97e968701bac6786
JuanDavidEscalanteCastañeda-Campus <juandaesca5@hotmail.com>
2023-06-28 09:48:18 -0500

    feat: :zap: CREACION DE COMMIT
    creacion de autoload (falta editar detalles), creacion de readme, base de datos, cracion de coneccion pdo y carpetas

---
+++ node/conversor.js
@@ -0,0 +1,11 @@
+
+
+export function convertirDolaresAEuros(dolares){
+let euro= 0.91;
+let conv=dolares*euro;
+}
+
+export function convertirDolaresALibras(dolares){
+    let libra= 0.78;
+    let conv=dolares*libra;
+}
