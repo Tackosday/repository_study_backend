1303308ceab3924dc6197db74d2f32bdb14fdfe4
JuanDavidEscalanteCastañeda-Campus <juandaesca5@hotmail.com>
2023-07-04 18:07:04 -0500

    feat: :tada: ASATEROID
    inicio de esqueleto del css y html eh iniciacion de backend

--- node/app.js
+++ node/app.js
@@ -1,15 +1,8 @@
 import {createServer} from 'http';
 import https from 'https';
 let myServer = createServer((req,res)=>{
-    let data="";
-    https.get(`https://pokeapi.co/api/v2/pokemon/1`,(peticion)=>{
-        peticion.on("data",(chunk)=>{
-            data += chunk
-        })
-        peticion.on("end",()=>{
-            res.end(data)
-        })
-    })
+
+    
 })
 
 
@@ -17,7 +10,7 @@ let myServer = createServer((req,res)=>{
 
 let config ={
     hostname:"172.16.51.107",
-    port:8080
+    port:3000   
 }
 
 myServer.listen(8080,()=>{
